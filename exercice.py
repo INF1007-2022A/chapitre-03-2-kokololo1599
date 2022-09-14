b52aecfda101c3f55ea9b0fc8115fa3e44ff4a53
kokololo1599 <112522946+kokololo1599@users.noreply.github.com>
2022-09-14 12:18:39 -0400

    Dang1

--- exercice.py
+++ exercice.py
@@ -11,25 +11,48 @@ def orthogonal(v1, v2):
 	v1[0] # Pour accéder au X
 	v1[1] # Pour accéder au Y
 	scalar_prod = v1[0] * v2[0] + v1[1] * v2[1]
-	return scalar_prod
+	if scalar_prod == 0:
+		return True
+	else:
+		return False
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	num_element = 0
+	sum = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v >= 0:
+			sum += v
+			num_element += 1
+
+
+	average = sum /num_element
+
+	return average
+
+		# La variable v contient une valeur de la liste.
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
 	while value != 0:
-		if value >= 20:
-			pass
-		elif value >= 10:
-			pass
-		elif value >= 5:
-			pass
-		elif value >= 1:
-			pass
+		twenties = 0
+		tens = 0
+		fives = 0
+		ones= 0
 
-	return (twenties, tens, fives, twos, ones);
+		while value >= 20:
+			value -= 20
+			twenties += 1
+		while value >= 10:
+			value -= 10
+			tens += 1
+		while value >= 5:
+			value -=5
+			fives += 1
+		while value >= 1:
+			value -= 1
+			ones += 1
+
+	return (twenties, tens, fives, ones);
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
